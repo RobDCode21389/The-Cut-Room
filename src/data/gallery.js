fe6d46fab0e51a26c5d81c5f489bcebcace3a1ce
Robert Davis <robdcode@yahoo.com>
2026-07-18 16:58:44 -0400

    Fix image filenames for case sensitivity

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,9 +1,9 @@
 import barber1 from '../assets/images/barber.jpg';
 import barber2 from '../assets/images/barber 2.jpg';
-import beard1 from '../assets/images/Beard 1.jpg';
-import beard2 from '../assets/images/Beard 2.jpg';
-import fade1 from '../assets/images/Fade 1.jpg';
-import fade2 from '../assets/images/Fade 2.jpg';
+import beard1 from '../assets/images/beard 1.jpg';
+import beard2 from '../assets/images/beard 2.jpg';
+import fade1 from '../assets/images/fade 1.jpg';
+import fade2 from '../assets/images/fade 2.jpg';
 
 // Export the array of 6 items
 export const galleryImages = [
